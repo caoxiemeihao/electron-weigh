ef66890f3a80946b76e03c678ad95dd409ee091e
草鞋没号 <308487730@qq.com>
2019-06-03 16:54:10 +0800

    19-06-03 添加dev环境判断

--- dist/电子称-win32-x64/resources/app/gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp')
-const electron = require('electron-connect').server.create()
-
-gulp.task('watch:electron', () => {
-    electron.start()
-    gulp.watch(['./*.js', './src/*.js'], electron.restart)
-    gulp.watch(['./front/css/*.css', './front/js/*.js', '.*.html'], electron.reload)
-})
